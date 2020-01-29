292114daef9e3d3f40ec6f72d25c04993bc4a783
JonasDutzi <jonas@astrid-dutzi.de>
2020-01-29 14:17:32 +0100

    Updates

---
+++ precache-manifest.1ab68daee9687933789fbcf368203636.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44303c272b71d41a72bc33d8d00cd396",
+    "url": "/pwa_react/index.html"
+  },
+  {
+    "revision": "2f18bddf110e88f56f31",
+    "url": "/pwa_react/static/css/main.d6a1a87c.chunk.css"
+  },
+  {
+    "revision": "65647498e74882c4b8bc",
+    "url": "/pwa_react/static/js/2.c375985c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/pwa_react/static/js/2.c375985c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2f18bddf110e88f56f31",
+    "url": "/pwa_react/static/js/main.91db173a.chunk.js"
+  },
+  {
+    "revision": "00822b582282771ae50a",
+    "url": "/pwa_react/static/js/runtime-main.bf20aca4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pwa_react/static/media/logo.5d5d9eef.svg"
+  }
+]);
