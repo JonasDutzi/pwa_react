ee36e77f670a10e79819ee6dbf05fd35761685dd
JonasDutzi <jonas@astrid-dutzi.de>
2020-01-29 14:12:55 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Online, Offline } from "react-detect-offline";
+import Btc from './Btc/Btc'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Real Time BTC tracker from blockchain.info
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          You are <Online>online</Online><Offline>offline</Offline>
+        </div>
+        <Btc />
       </header>
     </div>
   );
